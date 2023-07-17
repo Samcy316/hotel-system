798cf500585cf0f2a9bb3ff2ee79492dafb5c5a3
Samson Kamau Mwangi <samsonkamau316@gmail.com>
2023-07-17 15:04:10 +0300

    booking

--- src/app/bookingpage/bookingpage.component.ts
+++ src/app/bookingpage/bookingpage.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bookingpage',
   templateUrl: './bookingpage.component.html',
   styleUrls: ['./bookingpage.component.css']
 })
-export class BookingpageComponent {
+export class BookingpageComponent implements OnInit{
   paymentHandler: any = null;
-  constructor() {}
+  constructor(private formBuilder:FormBuilder) {}
+  bookingUser!:FormGroup;
+
+
+
+
   ngOnInit() {
     this.invokeStripe();
+    this.bookingUser=this.formBuilder.group({
+      firstname:['',Validators.required],
+      lastname:['',Validators.required],
+      email:[ '',[Validators.email]],
+      arrival:['',Validators.required],
+      departure:['', Validators.required],
+      special_request:['', Validators.required],
+      no_guest:['',Validators.required],
+      room_type:['', Validators.required]
+    })
 }
 makePayment(amount: any) {
   const paymentHandler = (<any>window).StripeCheckout.configure({
@@ -44,6 +61,11 @@ invokeStripe() {
     };
     window.document.body.appendChild(script);
   }
+}
+
+submitBooking(){
+  console.log(this.bookingUser)
+
 }
 
 }
--- src/app/bookpage/bookpage.component.ts
+++ src/app/bookpage/bookpage.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-bookpage',
   templateUrl: './bookpage.component.html',
   styleUrls: ['./bookpage.component.css']
 })
-export class BookpageComponent {
+export class BookpageComponent implements OnInit{
+
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
